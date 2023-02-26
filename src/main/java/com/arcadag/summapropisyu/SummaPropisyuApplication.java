31cacc66bdd2efb6e373abcc82f287b82530a36f
arcadag <piknik0711@yandex.ru>
2023-02-26 18:50:56 +0300

    initial commit

---
+++ src/main/java/com/arcadag/summapropisyu/SummaPropisyuApplication.java
@@ -0,0 +1,13 @@
+package com.arcadag.summapropisyu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SummaPropisyuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SummaPropisyuApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/arcadag/summapropisyu/SummaPropisyuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arcadag.summapropisyu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SummaPropisyuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
