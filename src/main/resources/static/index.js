f689d18600c063bc3035c9f99facea5538b9928c
arcadag <piknik0711@yandex.ru>
2023-03-08 22:55:13 +0300

    implemented the copy to clipboard function in index.js

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -11,16 +11,51 @@ angular.module('app', ['ngStorage']).controller('sumController', function ($scop
     };
 
     $scope.getResult = function (data) {
-        console.log(data);
         $http.post(sumContextPath, data)
             .then(function successCallback(response) {
-                console.log(response.data);
                 $scope.sumWriting = response.data;
 
             }, function errorCallback(){
                $scope.getResult($scope.defaultInfo);
             });
 
+    };
+
+    $scope.copyResult = function (result, event) {
+
+
+        var txtArea = document.createElement("textarea");
+        txtArea.id = 'txt';
+        txtArea.style.position = 'fixed';
+        txtArea.style.top = '0';
+        txtArea.style.left = '0';
+        txtArea.style.opacity = '0';
+        txtArea.value = result;
+        document.body.appendChild(txtArea);
+        txtArea.select();
+
+        try {
+            var successful = document.execCommand('copy');
+            var msg = successful ? 'successful' : 'unsuccessful';
+            console.log('Copying text command was ' + msg);
+            if (successful) {
+                $scope.changeAttr(event.target.id);
+                return true;
+            }
+        } catch (err) {
+            console.log('Oops, unable to copy');
+        } finally {
+            document.body.removeChild(txtArea);
+        }
+        return false;
+
+    };
+
+    $scope.changeAttr = function (id) {
+        console.log(id);
+        let img = angular.element(document.getElementById(id));
+        img.attr('src', "image/copied.png");
+        setTimeout(function(){ img.attr('src', "image/copy.png") }, 500);
     };
 
     $scope.getResult($scope.defaultInfo);
